4b8e8abdfa6b05a6874adb79529e72b2ffd6f8f6
truuue <noahvernhet@gmail.com>
2023-08-28 13:35:20 -0700

    pycodestyle fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,6 +2,7 @@
 """type-annotated function add that takes a float a and a float b as arguments
 and returns their sum as a float"""
 
+
 def add(a: float, b: float) -> float:
     """the add func"""
     return a + b
