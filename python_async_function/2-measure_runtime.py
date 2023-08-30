252e80916c553b8b9977db5a42b10b988bcc949a
truuue <noahvernhet@gmail.com>
2023-08-30 02:32:55 -0700

    pycodestyle fix

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """function should return a float"""
-wait_n = __import__('1-concurrent_coroutines').wait_n
 import asyncio
 import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
@@ -14,5 +14,5 @@ def measure_time(n: int, max_delay: int) -> float:
         end_time = time.time()
         total_time = end_time - start_time
         return total_time / n
-    
+
     return asyncio.run(async_measure())
