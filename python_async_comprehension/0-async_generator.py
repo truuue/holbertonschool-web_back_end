d517fe7ccd69ca354b8ab1b2decd25648f013c66
truuue <noahvernhet@gmail.com>
2023-08-30 03:23:13 -0700

    Fix the last checker with Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """coroutine will loop 10 times, each time asynchronously wait 1 second"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """random number between 0 and 10 func"""
     for _ in range(10):
         await asyncio.sleep(1)
