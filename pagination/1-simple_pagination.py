a3ed868a8973d4029405317d9f948782e2469238
truuue <noahvernhet@gmail.com>
2023-09-08 15:34:41 +0200

    Simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -19,8 +19,7 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -30,6 +29,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get_page function"""
         assert (isinstance(page, int) and isinstance(page_size, int)
         and page > 0 and page_size > 0)
-        return self.dataset()[index_range(page, page_size)]
+        self.dataset()
+        return [index_range(page, page_size)]
