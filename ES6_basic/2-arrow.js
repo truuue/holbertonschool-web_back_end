3909fd360fcabac1f7f7cdc212404f3921d192ef
truuue <noahvernhet@gmail.com>
2023-09-14 16:01:07 -0700

    eslint fix

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,8 +1,8 @@
-export default function getNeighborhoodsList () {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = (newNeighborhood) => {
+  this.addNeighborhood = function add(newNeighborhood) {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
