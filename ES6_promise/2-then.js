c8837185e276e4b11925c8645b5aa05bf65070bb
truuue <noahvernhet@gmail.com>
2023-10-06 01:17:31 -0700

    Catch me if you can!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,6 @@ export default function handleResponseFromAPI(promise) {
     return promise.then(() => ({
         status: 200,
         body: 'Success',
-    })).catch(() => Error()).finally(() => {
-        console.log('Got a response from the API');
-    });
+    })).catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
