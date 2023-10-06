bd964702151f47538b5c6e7c978f099f5ab47be9
truuue <noahvernhet@gmail.com>
2023-10-06 01:10:41 -0700

    Keep every promise you make and only make promises you can keep

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,6 +1,9 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      resolve();
-      reject();
-    });
-  }
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
