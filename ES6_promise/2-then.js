8323c13d757f06360db8ccb2f373870b2ac04d9c
truuue <noahvernhet@gmail.com>
2023-10-08 16:51:27 -0700

    Eslint fix

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-    return promise.then(() => ({
-        status: 200,
-        body: 'success',
-      })).then((response) => {
-        console.log('Got a response from the API');
-        return response;
-      }).catch(() => new Error());
-  }
+  return promise.then(() => ({
+    status: 200,
+    body: 'success',
+  })).then((response) => {
+    console.log('Got a response from the API');
+    return response;
+  }).catch(() => new Error());
+}
--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,6 +1,6 @@
 function signUpUser(firstName, lastName) {
   return Promise.resolve({
-    firstName: firstName,
-    lastName: lastName
+    firstName,
+    lastName,
   });
 }
