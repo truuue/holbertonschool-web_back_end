436bd35f55bacee8680417b0d8f723d259b19cd8
truuue <noahvernhet@gmail.com>
2023-10-09 01:49:54 -0700

    Eslint fix

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,9 @@
 export default function updateUniqueItems(groceries) {
-    const newGroceries = new Map();
-    groceries.forEach((value, key) => {
-        if (value === 1) {
-        newGroceries.set(key, value);
-        }
-    });
-    return newGroceries;
+  const newGroceries = new Map();
+  groceries.forEach((value, key) => {
+    if (value === 1) {
+      newGroceries.set(key, value);
+    }
+  });
+  return newGroceries;
 }
--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    const studentsByCity = students.filter((student) => student.location === city);
-    const studentsId = studentsByCity.map((student) => student.id);
-    const newStudents = students.map((student) => {
-        if (studentsId.includes(student.id)) {
-        const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
-        return { ...student, grade: studentGrade[0].grade };
-        }
-        return student;
-    });
-    return newStudents;
+  const studentsByCity = students.filter((student) => student.location === city);
+  const studentsId = studentsByCity.map((student) => student.id);
+  const newStudents = students.map((student) => {
+    if (studentsId.includes(student.id)) {
+      const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
+      return { ...student, grade: studentGrade[0].grade };
+    }
+    return student;
+  });
+  return newStudents;
 }
