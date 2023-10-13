b17562e8bad8ea52d166fb6bc45f9062e41a5eca
truuue <noahvernhet@gmail.com>
2023-10-13 12:06:55 -0700

    Checkers fix

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,6 +1,9 @@
-function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName,
-    lastName,
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve, reject) => {
+    resolve({
+      firstName,
+      lastName,
+    });
+    reject();
   });
 }
