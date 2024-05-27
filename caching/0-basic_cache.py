bb91afe5d172684139e789e10d53e05703efa021
truuue <noahvernhet@gmail.com>
2024-05-27 12:07:39 +0200

    [0. Basic dictionary] Try to fix the checker by adding some comments

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -5,10 +5,16 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """ BasicCache inherits from BaseCaching """
 
     def put(self, key, item):
+        """ put item in key cache """
         if key is not None or item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        return self.cache_data.get(key)
+        """get item from key cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data.get(key)
