3a91ca5bd49cca773f3dec3d00d5b4fb6523b09a
truuue <noahvernhet@gmail.com>
2024-05-27 13:58:14 +0200

    [1. FIFO caching] try another way to enjoy the checker

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-""" FIFO cache """
-
+"""FIFO Cache"""
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-
+    """FIFOCache class that inherits from BaseCaching"""
     def __init__(self):
-        """Constructor"""
         super().__init__()
         self.queue = []
 
     def put(self, key, item):
-        """put the item in key cache"""
-        if key and item:
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first_key = self.queue.pop(0)
-                del self.cache_data[first_key]
-                print(f"DISCARD: {first_key}\n")
-            self.queue.append(key)
-            self.cache_data[key] = item
+        """put method for put item in key"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_key = self.queue.pop(0)
+            del self.cache_data[first_key]
+            print(f"DISCARD: {first_key}\n")
+
+        self.queue.append(key)
+        self.cache_data[key] = item
 
     def get(self, key):
-        """get the item in key cache"""
+        """get method for getting item in key"""
         if key is None or key not in self.cache_data:
             return None
-        else:
-            self.cache_data(key)
+
+        return self.cache_data[key]
