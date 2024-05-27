e69d12946456138394da1aa2a0ca199efa548f2f
truuue <noahvernhet@gmail.com>
2024-05-27 13:50:51 +0200

    [1. FIFO caching] Forget some part of the task

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -5,16 +5,24 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
+
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        self.queue = []
+
     def put(self, key, item):
+        """put the item in key cache"""
         if key is not None or item is not None:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                del self.cache_data[first_key]
+                print(f"DISCARD: {first_key}\n")
+            self.queue.append(key)
             self.cache_data[key] = item
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key = next(iter(self.cache_data))
-            del self.cache_data[first_key]
-            print(f"DISCARD: {first_key}\n")
-
     def get(self, key):
+        """get the item in key cache"""
         if key is None or key not in self.cache_data:
             return None
         else:
