21baf3d69e7931b6b786b1b1f33b3338fb5b8269
truuue <noahvernhet@gmail.com>
2024-05-27 13:47:21 +0200

    [1. FIFO caching] adding a newline

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -12,7 +12,7 @@ class FIFOCache(BaseCaching):
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key = next(iter(self.cache_data))
             del self.cache_data[first_key]
-            print(f"DISCARD: {first_key}")
+            print(f"DISCARD: {first_key}\n")
 
     def get(self, key):
         if key is None or key not in self.cache_data:
