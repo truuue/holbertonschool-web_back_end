c75eb28882d312c709b73b2b8e6d15086b47cf9d
truuue <noahvernhet@gmail.com>
2024-05-28 10:07:07 +0200

    [3. LRU Caching] First try genius move ?

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" LRUCache """
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache class that inherits from BaseCaching """
+
+    def __init__(self):
+        """constructor"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """put method"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard_key = self.queue.pop(0)
+                del self.cache_data[discard_key]
+                print("DISCARD:", discard_key)
+
+        self.queue.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get method for getting item in key"""
+        if key is None or key not in self.cache_data:
+            return
+
+        return self.cache_data[key]
