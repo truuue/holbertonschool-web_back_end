884f19ab06334815e826864aeb596dfdc7ec6627
truuue <noahvernhet@gmail.com>
2024-05-28 11:04:10 +0200

    [4. MRU Caching] Try to fix Requirement

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -13,17 +13,20 @@ class MRUCache(BaseCaching):
         self.queue = []
 
     def put(self, key, item):
-        self.cache_data[key] = item
-
+        """put method"""
         if key is None or item is None:
             return
+
+        self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             discarded_key = self.queue.pop()
             del self.cache_data[discarded_key]
             print(f"DISCARD: {discarded_key}")
 
-        self.queue.append(key)  # Add the key to the queue
+        if key in self.queue:
+            self.queue.remove(key)
+        self.queue.append(key)
 
     def get(self, key):
         """Get method for getting item in key"""
