2a1fc6917883814dc486ebcf99462c86e339bc61
truuue <noahvernhet@gmail.com>
2024-06-19 14:58:01 +0200

    [1. Log formatter] Adding some comment to pass the checker

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,18 +6,19 @@ import logging
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ Constructor """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ Format the record """
         record.msg = filter_datum(
             self.fields, self.REDACTION, record.msg, self.SEPARATOR)
         return super().format(record)
