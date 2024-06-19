a7023dd301a2955cbe4f077d5206c10f67f1512f
truuue <noahvernhet@gmail.com>
2024-06-19 13:34:23 +0200

    [0. Regex-ing] Change the comment in filter_datum func

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,7 +7,16 @@ from typing import List
 def filter_datum(
         fields: List[str],
         redaction: str, message: str, separator: str) -> str:
-    """ Returns the log message obfuscated """
+    """
+    Filter the message by replacing the words in fields with the redaction
+    Args:
+        fields: a list of fields to replace in the message
+        redaction: a string representing the redacted message
+        message: a string representing the log line
+        separator: a string representing the separator of fields
+    Returns:
+        a string representing the log line
+    """
     pattern = '|'.join([f"{field}=.*?(?={separator}|$)" for field in fields])
     return re.sub(
         pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
