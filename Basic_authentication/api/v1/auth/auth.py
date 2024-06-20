e18095c193efa1b3fb6cbaedd28c087635b76722
truuue <noahvernhet@gmail.com>
2024-06-20 18:41:02 +0200

    [3. Auth class] Forgot the comment

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,18 +9,21 @@ class Auth:
     """ Auth class """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth """
         if excluded_paths and path in excluded_paths:
             return False
         else:
             return True
 
     def authorization_header(self, request=None) -> str:
+        """ authorization_header """
         if request is None or 'Authorization' not in request.headers:
             return None
         else:
             return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user """
         if request is None:
             return None
         else:
