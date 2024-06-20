e9b8dc3fe43d7d7982ab01ebd2bb43317fb907ed
truuue <noahvernhet@gmail.com>
2024-06-20 22:50:15 +0200

    [11. Basic - Overload current_user - and BOOM!] Fix the length for the pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -78,19 +78,11 @@ class BasicAuth(Auth):
 
 
 def current_user(self, request=None) -> TypeVar('User'):
+    """ Method that overloads Auth and retrieves the User instance
+    for a request
     """
-    Method that retrieves the User instance for a request
-    """
-    if request is None:
-        return None
-
-    authorization_header = self.authorization_header(request)
-    base64_authorization_header = self.extract_base64_authorization_header(
-        authorization_header)
-    decoded_base64_authorization_header = self.decode_base64_authorization_header(
-        base64_authorization_header)
-    user_email, user_pwd = self.extract_user_credentials(
-        decoded_base64_authorization_header)
-    user = self.user_object_from_credentials(user_email, user_pwd)
-
-    return user
+    auth_header = self.authorization_header(request)
+    base64_header = self.extract_base64_authorization_header(auth_header)
+    decoded_header = self.decode_base64_authorization_header(base64_header)
+    user, pwd = self.extract_user_credentials(decoded_header)
+    return self.user_object_from_credentials(user, pwd)
