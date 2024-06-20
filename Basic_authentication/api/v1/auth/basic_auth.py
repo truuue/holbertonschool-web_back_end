fd7f3a394996d535480ed18585bb06cace9fb3ee
truuue <noahvernhet@gmail.com>
2024-06-20 22:33:37 +0200

    [7. Basic - Base64 part] Pycodestyle fix

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,8 +7,14 @@ import base64
 class BasicAuth(Auth):
     """ Class to manage the API authentication """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        if authorization_header is None or not isinstance(authorization_header, str) or not authorization_header.startswith('Basic '):
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Method that extracts the base64 value from the Authorization header
+        """
+        if authorization_header is None or not isinstance(
+            authorization_header, str) or not authorization_header.startswith(
+                'Basic '):
             return None
 
         return authorization_header.split(' ')[1]
