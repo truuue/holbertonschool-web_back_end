b3146487980d102ba73e0b653a4ee56e7e26d0c8
truuue <noahvernhet@gmail.com>
2024-06-20 10:37:00 +0200

    [5. Encrypting passwords] Push the task file

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Encrypting passwords module """
+import bcrypt
+
+
+def hash_password(password):
+    """ Hash a password """
+    salt = bcrypt.gensalt()
+
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
