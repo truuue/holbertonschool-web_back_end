034b3304eadf390ee629879140767a8ed3338238
truuue <noahvernhet@gmail.com>
2024-06-20 22:32:34 +0200

    [7. Basic - Base64 part] Push the task file

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 """ Module of Basic Auth views """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ Class to manage the API authentication """
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None or not isinstance(authorization_header, str) or not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.split(' ')[1]
