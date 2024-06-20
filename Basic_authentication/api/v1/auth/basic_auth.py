74139c6e8086f941355ff95d1d989dc31df93dc4
truuue <noahvernhet@gmail.com>
2024-06-20 22:37:22 +0200

    [7. Basic - Base64 part] Fix the error "basic_auth.py:35:9: E722 do not use bare 'except'"

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of Basic Auth views """
 from api.v1.auth.auth import Auth
 import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -32,5 +33,5 @@ class BasicAuth(Auth):
             decoded_bytes = base64.b64decode(base64_authorization_header)
             decoded_string = decoded_bytes.decode('utf-8')
             return decoded_string
-        except:
+        except (binascii.Error, UnicodeDecodeError):
             return None
