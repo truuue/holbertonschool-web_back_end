06bd5f175b6249b2f03d4cf0b5fa7acdbca63634
truuue <noahvernhet@gmail.com>
2024-06-20 18:53:36 +0200

    [3. Auth class] Change the  comment length

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
+""" Module of Auth views """
 from flask import request
 from typing import List, TypeVar
 
-""" Module of Auth views """
-
 
 class Auth:
-    """ Auth class """
+    """ Class to manage the API authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require_auth """
+        """ Method for validating if endpoint requires auth """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ authorization_header """
+        """ Method that handles authorization header """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user """
+        """ A public method current_user """
         return None
