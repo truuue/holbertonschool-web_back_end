b74f8a14614a7522fe54039f31a3ea095f9184e8
truuue <noahvernhet@gmail.com>
2024-06-20 10:45:41 +0200

    [6. Check valid password] Push the task file

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check if a password is valid """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
