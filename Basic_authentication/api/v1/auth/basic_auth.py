26552a00511bb168a54e903a96a653951bc58d05
truuue <noahvernhet@gmail.com>
2024-06-20 22:42:24 +0200

    [10. Basic - User object] Push the task file

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,9 @@
 from api.v1.auth.auth import Auth
 import base64
 import binascii
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
+from typing import Type
 
 
 class BasicAuth(Auth):
@@ -52,3 +54,24 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> Type['User']:
+        """
+        Method that returns the User instance based on email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
