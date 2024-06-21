1ed03c7a61a546197834558f2e71baf1f7b3e92c
truuue <noahvernhet@gmail.com>
2024-06-21 11:39:03 +0200

    [7. New view for Session Authentication] Try to fix the checker

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -4,20 +4,23 @@ Route module for the API
 """
 from flask import Flask, jsonify, request
 from api.v1.views import app_views
-from api.v1.app import auth
 from models.user import User
-from models.user import User
+from os import getenv
+from api.v1.auth import auth
+
+app = Flask(__name__)
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login():
-    """ login module """
+def auth_session_login():
+    """ login session module """
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if not email:
+    if not email or email == "":
         return jsonify({"error": "email missing"}), 400
-    if not password:
+
+    if not password or password == "":
         return jsonify({"error": "password missing"}), 400
 
     user = User.search({'email': email})
@@ -28,9 +31,7 @@ def login():
         return jsonify({"error": "wrong password"}), 401
 
     session_id = auth.create_session(user[0].id)
-    user_dict = user[0].to_json()
-
-    response = jsonify(user_dict)
-    response.set_cookie(auth.SESSION_NAME, session_id)
+    response = jsonify(user[0].to_json())
+    response.set_cookie(getenv('SESSION_NAME'), session_id)
 
     return response
