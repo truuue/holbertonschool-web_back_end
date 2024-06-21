1c2bfb1e7d5babcf09753e7b6638a8281afef173
truuue <noahvernhet@gmail.com>
2024-06-21 11:02:19 +0200

    [2. Create a session] Push the task file

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,17 @@
 Route module for the API
 """
 from api.v1.auth.auth import Auth
-import os
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Authentication """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create session """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
