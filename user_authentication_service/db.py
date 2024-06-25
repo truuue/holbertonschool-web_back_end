8030d06c8a2064ee5c83b77a083e732c0ea22be4
truuue <noahvernhet@gmail.com>
2024-06-25 12:42:02 +0200

    [1. create user] Remove useless import and set the echo to false

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,8 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
-from user import User
+from user import Base, User
 
 
 class DB:
@@ -14,7 +13,7 @@ class DB:
 
     def __init__(self) -> None:
         """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
