93f0345e83a5720ab8552e457399bf8977afda33
truuue <noahvernhet@gmail.com>
2024-06-25 12:19:23 +0200

    [0. User model] Remove useless properties

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -10,8 +10,8 @@ class User(Base):
     """The User class for the user_authentication_service."""
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(250), nullable=False, unique=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
