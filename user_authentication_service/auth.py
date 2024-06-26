f65727970e646ff9728d0330b3a30e20388c83a9
truuue <noahvernhet@gmail.com>
2024-06-26 14:50:19 +0200

    [5. Register user] Push the task file

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ Hash password module """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +13,19 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_password
+
+
+class Auth:
+    """ Auth class to interact with the authentication database. """
+
+    def __init__(self):
+        """ Constructor method """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a user and return the User object """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
