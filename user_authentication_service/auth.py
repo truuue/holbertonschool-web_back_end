2ef15ea1ee9ca37e58a058c4d66e564deb520d61
truuue <noahvernhet@gmail.com>
2024-06-26 15:24:38 +0200

    [8. Credentials validation] Push the task file

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -29,3 +29,12 @@ class Auth:
             raise ValueError(f'User {email} already exists')
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if the login credentials are valid"""
+        try:
+            user = self._db.find_user_by(email=email)
+            hashed_password = user.hashed_password
+            return bcrypt.checkpw(password.encode(), hashed_password)
+        except NoResultFound:
+            return False
