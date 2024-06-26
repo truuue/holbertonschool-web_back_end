e1763aca85e30a20608050ec2797847c61e6939a
truuue <noahvernhet@gmail.com>
2024-06-26 14:11:08 +0200

    [4. Hash password] Push the task file

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Hash password module"""
+import bcrypt
+
+
+def _hash_password(password):
+    """ Module that hash password """
+    salt = bcrypt.gensalt()
+
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed_password
