40704910136744125c1692c972cce5db802371bd
truuue <noahvernhet@gmail.com>
2024-06-27 15:00:08 +0200

    [14. Log out] And another time x2

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -56,7 +56,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect("/")
+        return redirect("/", code=302)
     return abort(403)
 
 
