2317c573886cba6a60aab3eb626d68deef677e61
truuue <noahvernhet@gmail.com>
2024-06-27 14:27:38 +0200

    [12. Find user by session ID] Push the task file

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -53,3 +53,15 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Get a user by session ID. """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except Exception:
+            return None
+
+        return user
