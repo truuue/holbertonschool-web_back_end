3735e6787c3f80473802b5a743adb51f45223933
truuue <noahvernhet@gmail.com>
2024-07-04 15:56:52 +0200

    [2. Mock HTTP calls] Fix checker

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
-"""Generic utilities for github org client.
-"""
+"""Generic utilities for github org client."""
 import unittest
 from unittest import mock
-from utils import get_json
+from parameterized import parameterized
+from utils import access_nested_map, get_json
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test access_nested_map function."""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """Test access_nested_map function."""
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError("a")),
+        ({"a": 1}, ("a", "b"), KeyError("b"))
+    ])
+    def test_access_nested_map_exception(
+            self, nested_map, path, expected_exception):
+        """Test access_nested_map function for exception."""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), str(expected_exception))
 
 
 class TestGetJson(unittest.TestCase):
