5c2c7d47cd22e924c6b213b3a673c347d15067e7
truuue <noahvernhet@gmail.com>
2024-07-04 15:55:08 +0200

    [2. Mock HTTP calls]

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python3
-"""Generic utilities for github org client."""
+"""Generic utilities for github org client.
+"""
 import unittest
-from parameterized import parameterized
-from utils import access_nested_map
+from unittest import mock
+from utils import get_json
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    """Test access_nested_map function."""
+class TestGetJson(unittest.TestCase):
+    """Test get_json function."""
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        """Test access_nested_map function."""
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+    @mock.patch('utils.requests.get')
+    def test_get_json(self, mock_get):
+        """Test get_json function."""
+        test_url = "http://example.com"
+        test_payload = {"payload": True}
+        mock_response = mock.Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
 
-    @parameterized.expand([
-        ({}, ("a",), KeyError("a")),
-        ({"a": 1}, ("a", "b"), KeyError("b"))
-    ])
-    def test_access_nested_map_exception(
-            self, nested_map, path, expected_exception):
-        """Test access_nested_map function for exception."""
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), str(expected_exception))
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
