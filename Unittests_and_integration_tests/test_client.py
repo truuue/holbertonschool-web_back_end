94b6eab14de1bcbdfd050947c33c1a861b2a92af
truuue <noahvernhet@gmail.com>
2024-07-04 16:10:16 +0200

    [4. Parameterize and patch as decorators]

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Test client module."""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -22,6 +22,16 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, {"login": "test_org"})
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
+
+    def test_public_repos_url(self):
+        """Test GithubOrgClient._public_repos_url."""
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {
+                "repos_url": "https://api.github.com/orgs/test_org/repos"}
+            client = GithubOrgClient("test_org")
+            result = client._public_repos_url
+            self.assertEqual(
+                result, "https://api.github.com/orgs/test_org/repos")
 
 
 if __name__ == "__main__":
