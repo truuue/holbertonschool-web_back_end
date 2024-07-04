176e9975d31c63e1ba0ea2b61453060a303e6fef
truuue <noahvernhet@gmail.com>
2024-07-04 16:04:34 +0200

    [3. Parameterize and patch mandatory] pycodestyle fix

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -62,7 +62,8 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with mock.patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+        with mock.patch.object(
+                TestClass, 'a_method', return_value=42) as mock_method:
             test_instance = TestClass()
             # Call the memoized property twice
             result1 = test_instance.a_property
