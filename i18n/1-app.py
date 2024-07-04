3e021c6c377933c7169ab48a322415bc306f1e1c
truuue <noahvernhet@gmail.com>
2024-07-05 01:21:19 +0200

    [1. Basic Babel setup]

---
+++ i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Internationalization """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """ Home page """
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
