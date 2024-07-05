5da5482929e35bbedaef3e4721597de91dba2f2a
truuue <noahvernhet@gmail.com>
2024-07-05 11:00:43 +0200

    Refactor Babel setup and update index.html template

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,42 +1,31 @@
 #!/usr/bin/env python3
-"""
-Basic Babel setup
-"""
-
+""" App module """
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
-
-class Config(object):
-    """
-    Config class for Babel
-    """
+class Config:
+    """ App Config Class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object("1-app.Config")
-
-
-@babel.localeselector
-def get_locale():
-    """
-    Get the current locale
-    """
+def get_locale() -> str:
+    """ Find the best locale from request """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route("/")
-def index():
-    """
-    function that render index.html template
-    """
-    return render_template("1-index.html")
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    """ render index.html """
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
