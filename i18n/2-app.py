18fcd940efa7c45b154cf6519738cc7c82275c9f
truuue <noahvernhet@gmail.com>
2024-07-05 10:55:36 +0200

    Update render_template path and run app on all interfaces

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -2,6 +2,7 @@
 """
 Basic Babel setup
 """
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
@@ -34,8 +35,8 @@ def index():
     """
     function that render index.html template
     """
-    return render_template("2-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=5000)
