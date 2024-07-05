9006ad81d386c24201c58ba67161e67ad68e8abc
truuue <noahvernhet@gmail.com>
2024-07-05 10:34:57 +0200

    Try to pass the checker

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ Internationalization """
 from flask import Flask, render_template
-from flask_babel import Babel
+from flask_babel import Babel, get_locale, get_timezone
 
 app = Flask(__name__)
-
-babel = Babel(app)
+babel = Babel(app, locale_selector=get_locale, timezone_selector=get_timezone)
 
 
 class Config:
