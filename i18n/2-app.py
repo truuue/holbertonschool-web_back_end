cd145b2cb6ea424d9f23e1712445d6066f11b4eb
truuue <noahvernhet@gmail.com>
2024-07-05 10:45:44 +0200

    Update Babel setup and index route

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,24 +1,41 @@
 #!/usr/bin/env python3
-""" Internationalization """
-from flask import Flask, request
-from flask_babel import Babel, gettext
+"""
+Basic Babel setup
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
+class Config(object):
+    """
+    Config class for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object("1-app.Config")
+
+
 @babel.localeselector
 def get_locale():
-    """ Get locale """
-    return request.accept_languages.best_match(['en', 'fr', 'es'])
+    """
+    Get the current locale
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    """ Home page """
-    message = gettext('Hello, World!')
-    return message
+    """
+    function that render index.html template
+    """
+    return render_template("1-index.html")
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
