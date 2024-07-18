214097e8e2400f0811b3fa96b14653bb01c5f161
truuue <noahvernhet@gmail.com>
2024-07-18 12:08:14 +0200

    Refactor API port and update unit tests

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -1,14 +1,12 @@
 const express = require("express");
-
 const app = express();
-const port = 7865;
+
+const PORT = 7865;
 
 app.get("/", (req, res) => {
   res.send("Welcome to the payment system");
 });
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
-
-module.exports = app;
--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,24 +1,20 @@
 const request = require("request");
-const url = "http://127.0.0.1:7865";
+const chai = require("chai");
+const expect = chai.expect;
 
 describe("Index page", () => {
-  it("Good Status", (done) => {
-    request(url, (err, res, body) => {
-      assert.equal(res.statusCode, 200);
+  const url = "http://localhost:7865/";
+
+  it("Correct status code?", (done) => {
+    request.get(url, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
-  it("Correct output", (done) => {
-    request(url, (err, res, body) => {
-      assert.equal(body, "Welcome to the payment system");
-      done();
-    });
-  });
-
-  it("No error response", (done) => {
-    request(url, (err, res, body) => {
-      assert.equal(err, null);
+  it("Correct result?", (done) => {
+    request.get(url, (error, response, body) => {
+      expect(body).to.equal("Welcome to the payment system");
       done();
     });
   });
