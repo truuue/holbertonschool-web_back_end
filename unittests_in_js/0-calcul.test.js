ef8ca1f626dbac87d4144b7452a692fe03886b8e
truuue <noahvernhet@gmail.com>
2024-07-18 11:01:31 +0200

    Refactor calculateNumber function and add additional test cases

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,28 +1,42 @@
+// Create test case
+
 const assert = require("assert");
 const calculateNumber = require("./0-calcul.js");
 
-describe("calculateNumber", function () {
-  it("should return 4", () => {
+describe("calculateNumber", () => {
+  it("should return 4 when inputs are 1 and 3", function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it("should return 5", () => {
-    assert.strictEqual(calculateNumber(1.9, 3), 5);
-  });
-  it("should return 5", () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it("should return 6", () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+
+  it("should return 5 when inputs are 1 and 3.7", function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it("negative numbers", () => {
-    assert.strictEqual(calculateNumber(-1, 1), 0);
-    assert.strictEqual(calculateNumber(1, -1), 0);
-    assert.strictEqual(calculateNumber(-1, -1), -2);
+  it("should return 5 when inputs are 1.2 and 3.7", function () {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it("checks arguments", () => {
-    assert.strictEqual(isNaN(calculateNumber(1)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
+  it("should return 6 when inputs are 1.5 and 3.7", function () {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it("should return 0 when inputs are -0.4 and 0.4", function () {
+    assert.strictEqual(calculateNumber(-0.4, 0.4), 0);
+  });
+
+  it("should return -1 when inputs are -0.6 and -0.4", function () {
+    assert.strictEqual(calculateNumber(-0.6, -0.4), -1);
+  });
+
+  it("should return -2 when inputs are -1.4 and -0.6", function () {
+    assert.strictEqual(calculateNumber(-1.4, -0.6), -2);
+  });
+
+  it("should return -2 when inputs are -1.5 and -1.4", function () {
+    assert.strictEqual(calculateNumber(-1.5, -1.4), -2);
+  });
+
+  it("should return 1 when inputs are -1.5 and 2.4", function () {
+    assert.strictEqual(calculateNumber(-1.5, 2.4), 1);
   });
 });
