1b75a28d10c588adf8e49f3397ae8a4fe94de433
truuue <noahvernhet@gmail.com>
2024-08-05 11:57:13 +0200

    Update store method signature to accept multiple data types

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """ Writing strings to Redis """
 import redis
 import uuid
+from typing import Union
 
 
 class Cache:
@@ -12,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store data in Redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
