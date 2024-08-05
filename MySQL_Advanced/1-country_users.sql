d588bffecf744a3729bb28b758aff15217a4090b
truuue <noahvernhet@gmail.com>
2024-08-05 15:10:28 +0200

    Add table for unique users with country column

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Script to create a table with unique users
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
