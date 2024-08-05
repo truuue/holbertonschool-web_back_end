200f2010bf471d51b8558f6a25b197a59c1a1219
truuue <noahvernhet@gmail.com>
2024-08-05 15:08:34 +0200

    Refactor table creation query to use IF NOT EXISTS clause

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 -- Script to create a table with unique users
-CREATE TABLE
-    users IF NOT EXISTS (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        name VARCHAR(255)
-    );
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
